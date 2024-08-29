1d3689c65d679572fe95815387c409f3ea7dd340
Noah Hefner <noah.hefner127@peraton.com>
2024-08-29 14:42:34 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,25 @@
-const webpack = require('webpack');
 const path = require('path');
-const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: {
-        htmldiff: ['babel-polyfill', './src/Diff.js'],
-    },
-
-    output: {
-        filename: 'htmldiff.min.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
-        library: 'HtmlDiff',
-        libraryTarget: 'commonjs2'
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            }
-        ]
-    },
-
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new UnminifiedWebpackPlugin()
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
-
-    optimization: {
-        minimize: true
-    }
+  },
+  mode: 'development',
 };
